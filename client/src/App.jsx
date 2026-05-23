5846813125fe1bfa6ce78e42d7f96f02f89deeb3
Anshuman Panigrahi <panigrahia067@gmail.com>
2026-05-23 12:05:17 +0530

    Day-2 Task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+
+    axios.get("http://localhost:5000/api/test")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, []);
+
   return (
-    <div className="text-4xl font-bold text-center mt-20 text-blue-600">
-      Speech To Text App 🚀
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px"
+      }}
+    >
+      <h1>Speech To Text App 🚀</h1>
+
+      <h2>{message}</h2>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,9 +7,17 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Backend Running Successfully");
+    res.send("Backend Running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "API Connected Successfully ✅"
+    });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
